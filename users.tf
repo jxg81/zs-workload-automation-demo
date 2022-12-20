cfd202609571961c18278f12e3791b83f15cb488
Julian Greensmith <julian.greensmith@gmail.com>
2022-12-20 21:24:00 +1100

    testing

--- users.tf
+++ users.tf
@@ -6,16 +6,16 @@ data "zia_group_management" "package_management" {
   name = "package_management"
 }
 
-data "zia_group_management" "partner_sites" {
-  name = "partner_sites"
+data "zia_group_management" "security_services" {
+  name = "security_services"
 }
 
-resource "zia_user_management" "john_ashcroft" {
-  name         = "John Ashcroft"
-  email        = "john.ashcroft@acme.com"
+resource "zia_user_management" "erp_app" {
+  name         = "erp_app"
+  email        = "erp_app@workloads.local"
   password     = "P@ssw0rd123*"
   groups {
-    id = [data.zia_group_management.package_management.id, data.zia_group_management.partner_sites.id]
+    id = [data.zia_group_management.package_management.id, data.zia_group_management.security_services.id]
   }
   department {
     id = data.zia_department_management.workloads.id
