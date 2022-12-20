8ae4a8ac1f0ef212d40b7144758a5943730b6fd3
Julian Greensmith <julian.greensmith@gmail.com>
2022-12-20 21:13:02 +1100

    testing

--- users.tf
+++ users.tf
@@ -1,5 +1,23 @@
+data "zia_department_management" "workloads" {
+  name = "workloads"
+
+}
+data "zia_group_management" "package_management" {
+  name = "package_management"
+}
+
+data "zia_group_management" "partner_sites" {
+  name = "partner_sites"
+}
+
 resource "zia_user_management" "john_ashcroft" {
- name         = "John Ashcroft"
- email        = "john.ashcroft@acme.com"
- password     = "P@ssw0rd123*"
+  name         = "John Ashcroft"
+  email        = "john.ashcroft@acme.com"
+  password     = "P@ssw0rd123*"
+  groups {
+    id = [data.zia_group_management.package_management.id, data.zia_group_management.partner_sites.id]
+  }
+  department {
+    id = data.zia_department_management.workload.id
+  }
 }
