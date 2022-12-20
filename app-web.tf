5545c451c8d7b7a08023de04da4890dc5188f0e1
Julian Greensmith <julian.greensmith@gmail.com>
2022-12-20 22:56:53 +1100

    change rule order

--- app-web.tf
+++ app-web.tf
@@ -19,7 +19,7 @@ resource "zia_url_filtering_rules" "app_erp_rule" {
     description         = "allow erp app destinations"
     state               = "ENABLED"
     action              = "ALLOW"
-    order               = 2
+    order               = 4
     url_categories      = [resource.zia_url_categories.erp_allow_list.id]
     protocols           = ["ANY_RULE"]
     users {
