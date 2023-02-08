d048e701601e968dada1f6330351e278bb466806
Julian Greensmith <julian.greensmith@gmail.com>
2023-02-08 15:29:01 +1100

    added backend example for non-tf cloud

--- terraform/common.tf
+++ terraform/common.tf
@@ -1,4 +1,10 @@
 terraform {
+# Uncomment this backend config and update variables to store state files in remote location
+#    backend "s3" {
+#    bucket = "mybucket"
+#    key    = "path/to/my/key"
+#    region = "us-east-1"
+# }
   cloud {
   organization = "zphyrs"
     workspaces {
