792f4d96105b47045a3f59826219eb3a4ec31790
Julian Greensmith <julian.greensmith@gmail.com>
2023-03-09 14:34:56 +1100

    User name update

--- terraform/users.tf
+++ terraform/users.tf
@@ -19,7 +19,7 @@ data "zia_group_management" "security_services" {
 }
 
 data "zia_user_management" "julian" {
-  id = 58693867
+  name = "Julian Greensmith"
 }
 resource "zia_user_management" "erp_app" {
   name         = "erp_app"
