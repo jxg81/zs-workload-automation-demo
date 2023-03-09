5660a1a748308777cefff2609f06f7771676ce31
Julian Greensmith <julian.greensmith@gmail.com>
2023-03-09 14:13:23 +1100

    geet user with id

--- terraform/users.tf
+++ terraform/users.tf
@@ -19,7 +19,7 @@ data "zia_group_management" "security_services" {
 }
 
 data "zia_user_management" "julian" {
-  email = "julian@zphyrs.com"
+  id = 58693867
 }
 resource "zia_user_management" "erp_app" {
   name         = "erp_app"
