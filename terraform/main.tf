9e10be9c7f7d8a2c21c19a19e073bf378d7b05f7
Julian Greensmith <julian.greensmith@gmail.com>
2023-03-21 23:16:26 +1100

    Added comments to TF example to guide reuse

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,8 +6,10 @@ terraform {
 #    region = "us-east-1"
 # }
   cloud {
+  # Set this value to match your terrafrom cloud organisation name
   organization = "zphyrs"
     workspaces {
+      # Set this value to select the correct workspace in your terrafrom cloud organisation
       tags = ["zs-workload-automation-demo"]
     }
   }
@@ -22,11 +24,15 @@ terraform {
 provider "zia" {}
 
 locals {
+    # Change this domain name to match the domain used in your ZIA tenant which you would like the users created in
     domain = "zphyrs.com"
     apps_json_files = fileset(path.module, "./config/apps/*.json")   
     apps_data  = [ for f in local.apps_json_files : jsondecode(file("${path.module}/${f}")) ]
     users_data = csvdecode(file("./config/users.csv"))
 }
+# In this basic example the password for all users is set to the same value
+# The value is taken from an environment variable setting in the terrafrom cloud workspace called "PASSWORD"
+# In a real world deployment you would want to extract this data from a secure vault
 variable "PASSWORD" {
     type        = string
     description = "Password to ve used for all user creation"
