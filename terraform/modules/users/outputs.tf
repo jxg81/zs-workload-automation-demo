70f917142e0dffc4f6f3c9f33ca15e178f186047
Julian Greensmith <julian.greensmith@gmail.com>
2023-03-24 00:22:23 +1100

    fix typo

--- terraform/modules/users/outputs.tf
+++ terraform/modules/users/outputs.tf
@@ -1,3 +1,3 @@
 output "user_data" {
-  value = toset([resource.zia_user_management.users.id,resource.zia_user_management.users.name])
+  value = toset([resource.zia_user_management.user.id,resource.zia_user_management.user.name])
 }
