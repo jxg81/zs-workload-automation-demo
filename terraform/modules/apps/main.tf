d51ddb5e2b556075f35cccf0d2426b0ecdda16f2
Julian Greensmith <julian.greensmith@gmail.com>
2023-03-25 14:16:50 +1100

    troubleshooting

--- terraform/modules/apps/main.tf
+++ terraform/modules/apps/main.tf
@@ -35,25 +35,25 @@ resource "zia_firewall_filtering_destination_groups" "dstn_domain_group" {
   addresses = var.urls
 }
 
-resource "zia_firewall_filtering_rule" "firewall_rule" {
-    name                = join("-", [var.name, "application policy"])
-    description         = join(" ", ["Policy for application", var.name])
-    action              = "ALLOW"
-    state               = "ENABLED"
-    enable_full_logging = true
-    locations {
-      id = [for item in data.zia_location_management.location : item.id]
-    }
-    dest_ip_groups {
-      id = [resource.zia_firewall_filtering_destination_groups.dstn_domain_group.id]
-    }
-    nw_services {
-        id = [data.zia_firewall_filtering_network_service.http.id, data.zia_firewall_filtering_network_service.https.id]
-    }
-    users {
-        id = [tonumber(data.vault_generic_secret.user_data.data.id)]
-    }
-}
+#resource "zia_firewall_filtering_rule" "firewall_rule" {
+#    name                = join("-", [var.name, "application policy"])
+#    description         = join(" ", ["Policy for application", var.name])
+#    action              = "ALLOW"
+#    state               = "ENABLED"
+#    enable_full_logging = true
+#    locations {
+#      id = [for item in data.zia_location_management.location : item.id]
+#    }
+#    dest_ip_groups {
+#      id = [resource.zia_firewall_filtering_destination_groups.dstn_domain_group.id]
+#    }
+#    nw_services {
+#        id = [data.zia_firewall_filtering_network_service.http.id, data.zia_firewall_filtering_network_service.https.id]
+#    }
+#    users {
+#        id = [tonumber(data.vault_generic_secret.user_data.data.id)]
+#    }
+#}
 
 output "test_output1" {
   value = data.vault_kv_secret_v2.test_user_data.custom_metadata.email
