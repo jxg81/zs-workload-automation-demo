9ba8dc77bfe47c015d75452c7cd5fef5eebb355d
Julian Greensmith <julian.greensmith@gmail.com>
2023-03-25 12:16:46 +1100

    move id to secret store for easy access

--- terraform/modules/users/main.tf
+++ terraform/modules/users/main.tf
@@ -1,22 +1,22 @@
-  terraform {
+terraform {
   required_providers {
     zia = {
-      source  = "zscaler/zia"
+      source = "zscaler/zia"
     }
   }
-  }
+}
 
 resource "random_password" "user_password" {
-  length = 16
+  length  = 16
   special = true
   keepers = {
     user_name = var.name
   }
-} 
+}
 
 data "zia_group_management" "group" {
   for_each = var.groups
-  name = each.value
+  name     = each.value
 }
 
 data "zia_department_management" "workloads" {
@@ -24,9 +24,9 @@ data "zia_department_management" "workloads" {
 }
 
 resource "zia_user_management" "user" {
-  name         = var.name
-  email        = join("@", [var.name, var.domain])
-  password     = resource.random_password.user_password.result
+  name     = var.name
+  email    = join("@", [var.name, var.domain])
+  password = resource.random_password.user_password.result
   groups {
     id = [for item in data.zia_group_management.group : item.id]
   }
@@ -36,19 +36,20 @@ resource "zia_user_management" "user" {
 }
 
 resource "vault_kv_secret_v2" "vault_store_user_pass" {
-  mount                      = "kv"
-  name                       = var.name
-  delete_all_versions        = true
-  data_json                  = jsonencode(
-  {
-    password       = resource.zia_user_management.user.password,
-  }
+  mount               = "kv"
+  name                = var.name
+  delete_all_versions = true
+  data_json = jsonencode(
+    {
+      password = resource.zia_user_management.user.password,
+      id       = resource.zia_user_management.user.id
+    }
   )
-    custom_metadata {
+  custom_metadata {
     data = {
-      id = resource.zia_user_management.user.id
+
       username = resource.zia_user_management.user.name,
-      email = resource.zia_user_management.user.email
+      email    = resource.zia_user_management.user.email
     }
   }
 }
