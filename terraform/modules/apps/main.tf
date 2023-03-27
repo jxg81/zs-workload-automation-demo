a2995ac30b6ac5275a2d95aa171a3b8b6f69eda4
Julian Greensmith <julian.greensmith@gmail.com>
2023-03-27 13:26:44 +1100

    testing user vs ip restriction

--- terraform/modules/apps/main.tf
+++ terraform/modules/apps/main.tf
@@ -7,7 +7,6 @@ terraform {
 }
 
 data "vault_kv_secret_v2" "user_data" {
-  count = var.username == "" ? 0 : 1
   mount = var.vault_store
   name  = var.username
 }
@@ -40,6 +39,6 @@ resource "zia_firewall_filtering_rule" "firewall_rule" {
     id = [data.zia_firewall_filtering_network_service.http.id, data.zia_firewall_filtering_network_service.https.id]
   }
   users {
-    id = [tonumber(data.vault_kv_secret_v2.user_data.custom_metadata.id)]
+    id = [var.username == "" ? 0 : tonumber(data.vault_kv_secret_v2.user_data.custom_metadata.id)]
   }
 }
